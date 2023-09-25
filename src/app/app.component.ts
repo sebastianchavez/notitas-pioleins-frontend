3d9032c8225f73f3c9c9e5e7915e4293aa266801
Sebastián <seba.chavez21@gmail.com>
2023-09-25 17:43:37 -0300

    primer commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,6 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
-  title = 'notitas-pioleins-frontend';
+  title: string = 'notitas-pioleins-frontend';
+
 }
